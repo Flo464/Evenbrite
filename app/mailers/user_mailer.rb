7cd4bbfff73b19c69a6d30b2596ef7ea597a4d9a
Flora <caponi.flora@gmail.com>
2019-08-15 11:49:18 +0200

    debug mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < ApplicationMailer
     @user = user 
 
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url  = 'https://evening-shore-45925.herokuapp.com' 
+    @url  = 'https://vast-fortress-70962.herokuapp.com/' 
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject: 'Bienvenue chez nous !') 
